f31b82ca255ce88cec1f120eec4688fd230a1096
Đặng Châu <126049120+dangchau2111@users.noreply.github.com>
2024-08-01 21:28:50 +0700

    Update B3.py

--- Exercise_3/B3.py
+++ Exercise_3/B3.py
@@ -15,7 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Normalize the data
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X_test = scaler.fit_transform(X_test)
 
 class NeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size):
